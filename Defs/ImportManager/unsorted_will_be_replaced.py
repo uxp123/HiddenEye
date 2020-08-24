a87eda9074be2da27c1513fd344eb64ab29ea71e
Restyled.io <commits@restyled.io>
2020-08-24 09:41:28 +0000

    Restyled by isort

--- Defs/ImportManager/unsorted_will_be_replaced.py
+++ Defs/ImportManager/unsorted_will_be_replaced.py
@@ -8,35 +8,24 @@ import base64
 import getpass
 import platform
 import re as regular_expression
+import signal
 import socket
 import stat
 from distutils.dir_util import copy_tree as webpage_set
 from io import BytesIO
-from os import chdir
-from os import chmod
-from os import getuid
-from os import mkdir
-from os import path
-from os import remove
-from os import replace
-from os import system
-from os import kill
+from os import chdir, chmod, getuid, kill, mkdir, path, remove, replace, system
 from pathlib import Path as pathlib_Path
-from shutil import copyfile
-from shutil import rmtree
+from shutil import copyfile, rmtree
+from subprocess import DEVNULL, PIPE, CalledProcessError
+from subprocess import Popen as run_background_command
 from subprocess import call as run_command
-from subprocess import CalledProcessError
 from subprocess import check_call as try_to_run_command
 from subprocess import check_output
-from subprocess import DEVNULL
-from subprocess import PIPE
-from subprocess import Popen as run_background_command
 from time import sleep as wait
 from urllib import request as url_request
 from zipfile import ZipFile
-from pgrep import pgrep as check_process
 
 import requests
-import signal
+from pgrep import pgrep as check_process
 from pyngrok import conf as ngrok_conf
 from pyngrok import ngrok
